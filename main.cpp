85b8a6cc229f43f210e0eb867562b2eba8db85d1
noahdominic <noahdmnc@gmail.com>
2025-12-22 20:48:01 +0800

    Integrate day 2 to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "day01.hpp"
+#include "day02.hpp"
+#include "utils.hpp"
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -9,6 +11,7 @@ int main(int argc, char** argv)
     std::cout << "Hello" << std::endl;
     std::cout << day01_part1() << std::endl;
     std::cout << day01_part2() << std::endl;
-    
+    std::cout << day02_part1() << std::endl;
+
     return 0;
 }
