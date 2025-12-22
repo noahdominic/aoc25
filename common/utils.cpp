e12491ba63ad05fbc7edcfda2de81575e10b6340
noahdominic <noahdmnc@gmail.com>
2025-12-22 20:38:14 +0800

    New util: `split` for splitting strings with delimiters

--- common/utils.cpp
+++ common/utils.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -17,5 +18,28 @@ namespace utils
         }
 
         return lines;
+    }
+
+    /// split
+    ///
+    /// by Arafat Hasan, edited by Quonux
+    /// https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+    ///
+    /// Split string based on a string delimiter. Such as splitting string "adsf-+qwret-+nvfkbdsj-+orthdfjgh-+dfjrleih" based on string delimiter "-+", output will be {"adsf", "qwret", "nvfkbdsj", "orthdfjgh", "dfjrleih"}
+    std::vector<std::string>
+    split(std::string s, std::string delimiter)
+    {
+        size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+        std::string token;
+        std::vector<std::string> res;
+
+        while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+            token = s.substr (pos_start, pos_end - pos_start);
+            pos_start = pos_end + delim_len;
+            res.push_back (token);
+        }
+
+        res.push_back (s.substr (pos_start));
+        return res;
     }
 }
--- include/utils.hpp
+++ include/utils.hpp
@@ -6,4 +6,5 @@
 namespace utils
 {
     std::vector<std::string> read_file(std::string file_path);
+    std::vector<std::string> split(std::string s, std::string delimiter);
 }
