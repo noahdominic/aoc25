e7ba544df8178048d4c590068e9deaf0f82a88b2
noahdominic <noahdmnc@gmail.com>
2025-12-25 22:59:32 +0800

    Solve day 6 part 2

--- days/day06.cpp
+++ days/day06.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include "utils.hpp"
 
 enum
@@ -52,15 +51,89 @@ day06_part1()
                 : column_result * grid[row][col];
         }
         
-        std::cout << "At column " << col << ", the total is: " << column_result << std::endl;
         total += column_result;
     }
 
     return total;
 }
 
+std::string
+extract_column(const std::vector<std::string>& data, size_t col)
+{
+    std::string result;
+    result.reserve(data.size());
+
+    for (const std::string& str : data) {
+        if (col < str.size()) {
+            result += str[col];
+        }
+    }
+
+    return result;
+}
+
+std::vector<long>
+extract_operands(std::vector<std::string>& data,
+    size_t col,
+    int offset)
+{
+    std::vector<long> operands({});
+
+    while (offset >= 0)
+    {
+        operands.emplace_back(std::stol(extract_column(data, col + offset)));
+        --offset;
+    }
+
+    return operands;
+}
+
 long
 day06_part2()
 {
-    return 0;
+    long total = 0;
+    std::vector<std::string> data = utils::read_file("input/day06.txt");
+    std::string last_row = data.back();
+    data.pop_back(); // important, so we can do loops on data w/o having to worry abt the last row
+    std::vector<operation> operations({last_row[0] == '+' ? ADD : MULTIPLY});
+    std::vector<size_t> operation_locations({0});
+    std::vector<size_t> operation_offsets;
+
+    // Skip first item; it's been done above at declaration
+    for (size_t i = 1; i < last_row.size(); ++i)
+    {
+        if (last_row[i] != ' ')
+        {
+            operations.push_back(last_row[i] == '+' ? ADD : MULTIPLY);
+            operation_locations.push_back(i);
+        }
+    }
+    
+    // Should check if more than 1 item but realistically don't have to
+    // From start to penultimate since we're doing index + 1
+    for (size_t i = 0; i + 1 < operation_locations.size(); ++i)
+    {
+        operation_offsets.push_back(operation_locations[i + 1] - operation_locations[i] - 2);
+    }
+
+    // Last item
+    operation_offsets.push_back(last_row.size() - operation_locations.back() - 1);
+
+    for (size_t current_op_index = 0; current_op_index < operation_locations.size(); ++current_op_index)
+    {
+        // Operands are numbers to be operated
+        std::vector<long> operands = extract_operands(data, operation_locations[current_op_index], operation_offsets[current_op_index]);
+
+        // Should check if operands are not empty but don't have to realistically
+        long sub_total = operands[0];
+        for (size_t i = 1; i < operands.size(); ++i)
+        {
+            sub_total = (operations[current_op_index] == ADD) ? sub_total + operands[i] : sub_total * operands[i];
+        }
+
+        total += sub_total;
+
+    }
+
+    return total;
 }
