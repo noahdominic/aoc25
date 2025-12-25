3be760678d978530f0f2ac948fd6147d2fae965f
noahdominic <noahdmnc@gmail.com>
2025-12-25 22:59:52 +0800

    Write extension to make vector printable

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,6 +2,18 @@
 
 #include <string>
 #include <vector>
+#include <ostream>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    os << "[";
+    for (std::size_t i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i + 1 < v.size()) os << ", ";
+    }
+    os << "]";
+    return os;
+}
 
 namespace utils
 {
