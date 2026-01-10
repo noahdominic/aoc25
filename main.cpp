a0bec33b0bd42a4daff51c88a67e51ca2e5d86e1
noahdominic <noahdmnc@gmail.com>
2026-01-10 19:13:31 +0800

    Clean up main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,35 @@
 #include "day01.hpp"
 #include "day02.hpp"
 #include "day03.hpp"
-#include "day05.hpp"
 #include "day04.hpp"
+#include "day05.hpp"
 #include "day06.hpp"
 #include "day07.hpp"
+#include "day08.hpp"
 #include "utils.hpp"
 #include <iostream>
 
-int main(int argc, char** argv)
-{
-    // int day = std::stoi(argv[1]);
-    // int part = std::stoi(argv[2]);
+int main(int argc, char **argv) {
+  // int day = std::stoi(argv[1]);
+  // int part = std::stoi(argv[2]);
 
-    std::cout << "Hello" << std::endl;
-    // std::cout << day01_part1() << std::endl;
-    // std::cout << day01_part2() << std::endl;
-    // std::cout << day02_part1() << std::endl;
-    // std::cout << day02_part2() << std::endl;
-    // std::cout << day03_part1() << std::endl;
-    // std::cout << day03_part2() << std::endl;
-    // std::cout << day04_part1() << std::endl;
-    // std::cout << day04_part2() << std::endl;
-    // std::cout << day05_part1() << std::endl;
-    // std::cout << day05_part2() << std::endl;
-    // std::cout << day06_part1() << std::endl;
-    // std::cout << day06_part2() << std::endl;
-    // std::cout << day07_part1() << std::endl;
-    // std::cout << day07_part2() << std::endl;
+  std::cout << "Hello" << std::endl;
+  // std::cout << day01_part1() << std::endl;
+  // std::cout << day01_part2() << std::endl;
+  // std::cout << day02_part1() << std::endl;
+  // std::cout << day02_part2() << std::endl;
+  // std::cout << day03_part1() << std::endl;
+  // std::cout << day03_part2() << std::endl;
+  // std::cout << day04_part1() << std::endl;
+  // std::cout << day04_part2() << std::endl;
+  // std::cout << day05_part1() << std::endl;
+  // std::cout << day05_part2() << std::endl;
+  // std::cout << day06_part1() << std::endl;
+  // std::cout << day06_part2() << std::endl;
+  // std::cout << day07_part1() << std::endl;
+  // std::cout << day07_part2() << std::endl;
 
-    return 0;
+  std::cout << day08_part1() << std::endl;
+  std::cout << day08_part2() << std::endl;
+  return 0;
 }
